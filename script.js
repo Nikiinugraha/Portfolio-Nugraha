4e2465048a3b3988f0f1938638bf6e80fffd85bd
Nikiinugraha <162570479+Nikiinugraha@users.noreply.github.com>
2024-11-08 19:03:11 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const hamburger_icon = document.getElementById("hamburger-icon");
+const menuList = document.getElementById("menu-list");
+
+hamburger_icon.addEventListener("click", () => {
+    menuList.classList.toggle("hidden");
+});
+
+
+// BOX PENGALAMAN
+
+
